2d2817212c51570238bae9b468deaf2d083e6a31
FernandoMMarina <61161806+FernandoMMarina@users.noreply.github.com>
2021-11-20 10:42:11 -0300

    Update

--- src/components/articulos/HomeComponent.js
+++ src/components/articulos/HomeComponent.js
@@ -1,31 +1,20 @@
 import { Link } from "react-router-dom";
 import { ArticuloAdd } from "./ArticuloAdd";
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
-export default function ArticuloComponent(props) {
+
+import React from 'react';
+ 
+
+export default function HomeComponent(props) {
 
     const {articulo} = props;
-
+    
     return (
         <>
-            <div className="col-xs-auto col-sm-auto col-md-6 col-lg-2 col-xxl-2 text-center">
-                <div className="card h-100">
-                    <img src="/assets/images/ceramica.jpg" className="card-img-top" alt="ceramica" />
-                    <div className="card-body">
-                        <Typography color="textSecondary"  variant="h5" gutterBottom>
-                            {articulo.titulo}
-                         </Typography>
-                        <p className="card-text">${articulo.precio}</p>
-                        <Link to={`/articulo/detalle/${articulo.id}`}>
-                            <Button variant="contained" color="primary">Caracteristicas</Button>
-                        </Link>
-                    </div>
-                    <div className="card-footer">
-                        <ArticuloAdd articulo={articulo}></ArticuloAdd>
-                    </div>
-                </div>
-            </div>
+              <div>
+               
+              </div>
         </>
     )
 }
